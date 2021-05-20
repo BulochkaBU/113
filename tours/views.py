0ea54c115b716b9f12f4a3ac2d89bd108ddf7016
Anastasia <nastyaerm@mail.ru>
2021-05-20 21:06:14 +0300

    add views.py

--- tours/views.py
+++ tours/views.py
@@ -3,17 +3,17 @@ from django.http import HttpResponseNotFound, HttpResponseServerError
 import random
 from tours import data
 
+
 # Create your views here.
+
 def main_view(request):
     random_title_hotels_dict = dict(random.sample(data.tours.items(), 6))
-
     return render(request, 'index.html', context={
         'title': data.title,
         'subtitle': data.subtitle,
         'description': data.description,
         'departures': data.departures,
         'hotel_title': random_title_hotels_dict.values(),
-
 
     })
 
@@ -36,9 +36,6 @@ def departure_view(request, departure):
     min_price = min(prices)
     max_price = max(prices)
 
-
-
-
     return render(request, 'departure.html', context={
         'departure': dep,
         'dep_tours': dep_tours,
@@ -54,7 +51,7 @@ def tour_view(request, tour_id):
     tour = data.tours[tour_id]
     tour_departure = data.departures[tour['departure']]
     stars = int(tour['stars'])
-    for star in range(stars+1):
+    for star in range(stars + 1):
         star *= '★'
     return render(request, 'tour.html', context={
         'tour': tour,
